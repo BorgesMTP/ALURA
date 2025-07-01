106f54f81e81b65e13b3b04ff412505c023ff487
Marco Túlio Borges <marcotpb@gmail.com>
2025-07-01 17:17:17 -0300

    Adição do projeto "Ingresso Online"

---
+++ ingresso/js/app.js
@@ -0,0 +1,49 @@
+document.getElementById('qtd').value = 0;
+
+function comprar() {
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+    } else if (tipo.value == 'superior') {
+        comprarSuperior(qtd);
+    } else if (tipo.value == 'inferior') {
+        comprarInferior(qtd);
+    }
+
+    document.getElementById('qtd').value = 0;
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para este tipo de ingresso');
+    } else {
+        qtdPista -= qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível para este tipo de ingresso');
+    } else {
+        qtdSuperior -= qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponível para este tipo de ingresso');
+    } else {
+        qtdInferior -= qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra realizada com sucesso!');
+    }
+}
